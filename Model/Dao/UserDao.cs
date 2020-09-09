8c78e234a50471ddc94e210ea7db3b73e1f8df2f
wujor01 <32138284+wujor01@users.noreply.github.com>
2020-09-09 18:34:49 +0700

    Thêm tính năng: Cập nhật thông tin user, xóa user

--- Model/Dao/UserDao.cs
+++ Model/Dao/UserDao.cs
@@ -10,18 +10,44 @@ namespace Model.Dao
 {
     public class UserDao
     {
+        //Khai báo db
         WebAppDbContext db = null;
         public UserDao()
         {
             db = new WebAppDbContext();
         }
+        //Thêm người dùng
         public long Insert(User entity)
         {
             db.User.Add(entity);
             db.SaveChanges();
             return entity.ID;
         }
-
+        //Cập nhật thông tin User
+        public bool Update(User entity)
+        {
+            try
+            {
+                var user = db.User.Find(entity.ID);
+                user.Email = entity.Email;
+                if (!string.IsNullOrEmpty(entity.Password))
+                {
+                    user.Password = entity.Password;
+                }
+                user.TimeStart = entity.TimeStart;
+                user.TimeOut = entity.TimeOut;
+                user.ModifiedBy = entity.ModifiedBy;
+                user.ModifiedDate = DateTime.Now;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //logging
+                return false;
+            }
+        }
+        //Phân trang quản lý user và thêm mục tìm kiếm theo username và email
         public IEnumerable<User> ListAllPaging(string searchString, int page, int pageSize)
         {
             IQueryable<User> model = db.User;
@@ -32,10 +58,72 @@ namespace Model.Dao
 
             return model.OrderByDescending(x => x.CreatedDate).ToPagedList(page, pageSize);
         }
+        //Kiểm tra phiên đăng nhập có hợp lệ không
         public User GetById(string userName)
         {
             return db.User.SingleOrDefault(x => x.Username == userName);
         }
+
+        public User ViewDetail(int id)
+        {
+            return db.User.Find(id);
+        }
+
+        //Kiểm tra user đang nhập có quyền thực hiện các tác vụ thêm, sửa, xóa theo phân quyền
+
+        //public List<string> GetListCredential(string userName)
+        //{
+        //    var user = db.User.Single(x => x.Username == userName);
+        //    var data = (from a in db.Employee
+        //                join b in db.UserGroups on a.UserGroupID equals b.ID
+        //                join c in db.Roles on a.RoleID equals c.ID
+        //                where b.ID == user.GroupID
+        //                select new
+        //                {
+        //                    RoleID = a.RoleID,
+        //                    UserGroupID = a.UserGroupID
+        //                }).AsEnumerable().Select(x => new Credential()
+        //                {
+        //                    RoleID = x.RoleID,
+        //                    UserGroupID = x.UserGroupID
+        //                });
+        //    return data.Select(x => x.RoleID).ToList();
+
+        //}
+
+        public bool ChangeStatus(long id)
+        {
+            var user = db.User.Find(id);
+            user.Status = !user.Status;
+            db.SaveChanges();
+            return user.Status;
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var user = db.User.Find(id);
+                db.User.Remove(user);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+        public bool CheckUserName(string userName)
+        {
+            return db.User.Count(x => x.Username == userName) > 0;
+        }
+        public bool CheckEmail(string email)
+        {
+            return db.User.Count(x => x.Email == email) > 0;
+        }
+
         public int Login(string userName, string passWord)
         {
             var result = db.User.SingleOrDefault(x => x.Username == userName);
--- WebApp/Areas/Admin/Controllers/UserController.cs
+++ WebApp/Areas/Admin/Controllers/UserController.cs
@@ -14,6 +14,8 @@ namespace WebApp.Areas.Admin.Controllers
     public class UserController : BaseController
     {
         // GET: Admin/User
+
+        //[HasCredential(RoleID = "VIEW_USER")]
         public ActionResult Index(string searchString, int page = 1, int pageSize = 10)
         {
             var dao = new UserDao();
@@ -23,22 +25,81 @@ namespace WebApp.Areas.Admin.Controllers
 
             return View(model);
         }
+
+        //[HasCredential(RoleID = "EDIT_USER")]
+        public ActionResult Edit(int id)
+        {
+            var user = new UserDao().ViewDetail(id);
+            return View(user);
+        }
+
+        [HttpPost]
+        //[HasCredential(RoleID = "EDIT_USER")]
+        public ActionResult Edit(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new UserDao();
+                if (!string.IsNullOrEmpty(user.Password))
+                {
+                    var encryptedMd5Pas = Encryptor.MD5Hash(user.Password);
+                    user.Password = encryptedMd5Pas;
+                }
+
+
+                var result = dao.Update(user);
+                if (result)
+                {
+                    SetAlert("Cập nhật user thành công", "success");
+                    return RedirectToAction("Index", "User");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Cập nhật user không thành công");
+                }
+            }
+            return View("Index");
+        }
+
+        [HttpDelete]
+        //[HasCredential(RoleID = "DELETE_USER")]
+        public ActionResult Delete(int id)
+        {
+            new UserDao().Delete(id);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        //[HasCredential(RoleID = "EDIT_USER")]
+        public JsonResult ChangeStatus(long id)
+        {
+            var result = new UserDao().ChangeStatus(id);
+            return Json(new
+            {
+                status = result
+            });
+        }
+
         [HttpGet]
+        //[HasCredential(RoleID = "ADD_USER")]
         public ActionResult Create()
         {
             return View();
         }
+
         [HttpPost]
+        //[HasCredential(RoleID = "ADD_USER")]
         public ActionResult Create(User user)
         {
             if (ModelState.IsValid) 
             {
             var dao = new UserDao();
-
+            //Băm mật khẩu bằng hàm MD5
             var encryptedMd5Pas = Encryptor.MD5Hash(user.Password);
             user.Password = encryptedMd5Pas;
 
-                //lấy id trong session đăng nhập của mod lưu vào phiên tạo mới user
+                //lấy id trong session đăng nhập của quản trị lưu vào phiên tạo mới user
                 var session = (UserLogin)Session[CommonConstants.USER_SESSION];
                 user.CreatedBy = session.UserName;
                 user.CreatedDate = DateTime.Now;
